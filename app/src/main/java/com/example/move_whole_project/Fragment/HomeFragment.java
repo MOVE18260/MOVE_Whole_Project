c649eeda1198d5aca10eb2d4f3204125f39706d6
Jihoon Kim <ghzzzang@naver.com>
2023-05-14 19:49:36 +0900

    feat: step_dino_animation add

--- app/src/main/java/com/example/move_whole_project/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/move_whole_project/Fragment/HomeFragment.java
@@ -1,14 +1,17 @@
 package com.example.move_whole_project.Fragment;
 
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,8 +31,11 @@ public class HomeFragment extends Fragment implements SensorEventListener {
     // 걸음수 측정을 위한 변수
 
     int stepCnt = 0;
-    int totalSteps = 0;
+    int totalCnt = 0;
     TextView tv_stepCnt;
+    ImageView iv_dino;
+    AnimationDrawable an_dino;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,40 +45,57 @@ public class HomeFragment extends Fragment implements SensorEventListener {
 
 
         tv_stepCnt = (TextView) view.findViewById(R.id.tv_stepCnt);
+        iv_dino = (ImageView) view.findViewById(R.id.iv_dino);
+        an_dino = (AnimationDrawable) iv_dino.getBackground();
+
         // 걸음수 측정 센서:
         sensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
-        stepCntSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        stepCntSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
         // 센서 존재 여부 확인
         if(stepCntSensor == null){
             Toast.makeText(getContext(), "걸음수 측정 센서 없음", Toast.LENGTH_SHORT).show();
         }
         // 어떤 프레그먼트로 돌아가면 계속 초기화되어서 보이는데 그렇게 보이지 않게 하기 위한 코드
-        if(totalSteps != 0){
-            tv_stepCnt.setText(String.valueOf(totalSteps));
+        if(totalCnt != 0){
+            tv_stepCnt.setText(String.valueOf(totalCnt));
         }
         // Inflate the layout for this fragment
         return view;
     }
-
 
     public void onStart(){
         super.onStart();
         // 센서가 존재한다면
         if(stepCntSensor != null){
             // 센서 속도 설정
-            sensorManager.registerListener(this, stepCntSensor,SensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(this, stepCntSensor,SensorManager.SENSOR_DELAY_UI);
         }
     }
+
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         // 걸음 센서 이벤트 발생시
-        if(sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_STEP_DETECTOR){
                 // 센서 이벤트가 발생할때 마다 걸음 수 증가
-                stepCnt++;
-                totalSteps = stepCnt;
-                tv_stepCnt.setText(String.valueOf(totalSteps));
+                if(sensorEvent.values[0] ==1.0f){
+                    stepCnt++;
+                    totalCnt = stepCnt;
+                    tv_stepCnt.setText(String.valueOf(totalCnt));
+                    an_dino.start();
+                    new Handler().postDelayed(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            an_dino.stop();
+                            //딜레이 후 시작할 코드 작성
+                        }
+                    }, 2000);// 2초 정도 딜레이를 준 후 시작
+
+                }
+
         }
     }
 
